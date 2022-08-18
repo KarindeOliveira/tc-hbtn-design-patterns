9b5f8567486bfd3b41cbda0f6dc42403b62fbcb1
Karin Pinheiro <karindanoli@gmail.com>
2022-08-18 16:51:32 -0300

    Testes

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,20 @@
+public class NaveEspacialDecorator extends NaveEspacial{
+
+    private NaveEspacial naveDecorada;
+
+    public NaveEspacialDecorator(NaveEspacial naveDecorada) {
+        super(naveDecorada.getSaude(), naveDecorada.getAtaque());
+        this.naveDecorada = naveDecorada;
+    }
+
+    @Override
+    public int getAtaque() {
+        return this.naveDecorada.getAtaque();
+    }
+
+    @Override
+    public int getSaude() {
+        return this.naveDecorada.getSaude();
+    }
+
+}
